5d81629a4ea6afcc30e2a4ec947c4089e4522cb0
felipetodev <fe.ossandon.u@gmail.com>
2023-09-21 20:48:15 -0300

    fix: reduce btn size for icons variant

--- apps/web/src/components/codeblock.tsx
+++ apps/web/src/components/codeblock.tsx
@@ -98,7 +98,7 @@ const CodeBlock: FC<Props> = memo(({ language, value }) => {
         <span className="text-xs lowercase">{language}</span>
         <div className="flex items-center space-x-1">
           <Button
-            className="hover:bg-zinc-800 focus-visible:ring-1 focus-visible:ring-slate-700 focus-visible:ring-offset-0"
+            className="h-7 w-7 hover:bg-zinc-800 focus-visible:ring-1 focus-visible:ring-slate-700 focus-visible:ring-offset-0"
             onClick={downloadAsFile}
             size="icon"
             variant="ghost"
@@ -107,7 +107,7 @@ const CodeBlock: FC<Props> = memo(({ language, value }) => {
             <span className="sr-only">Download</span>
           </Button>
           <Button
-            className="text-xs hover:bg-zinc-800 focus-visible:ring-1 focus-visible:ring-slate-700 focus-visible:ring-offset-0"
+            className="h-7 w-7 text-xs hover:bg-zinc-800 focus-visible:ring-1 focus-visible:ring-slate-700 focus-visible:ring-offset-0"
             onClick={onCopy}
             size="icon"
             variant="ghost"
