be6fcf220d26e8669f07aca8fccf065889f946f1
felipetodev <fe.ossandon.u@gmail.com>
2023-09-21 19:15:02 -0300

    update rules

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -1,10 +1,17 @@
 module.exports = {
   extends: ["custom/next"],
   rules: {
+    "eslint-comments/require-description": "off",
+    "@typescript-eslint/consistent-type-definitions": "off",
+    "react/no-unstable-nested-components": "off",
+    "@typescript-eslint/no-unsafe-member-access": "off",
+    "@typescript-eslint/naming-convention": "off",
+    "@typescript-eslint/no-unnecessary-condition": "off",
     "@typescript-eslint/explicit-function-return-type": "off",
     "import/no-extraneous-dependencies": "off",
     "@typescript-eslint/no-unsafe-assignment": "off",
     "@typescript-eslint/no-non-null-assertion": "off",
     "@typescript-eslint/no-unsafe-call": "off",
+    "prefer-named-capture-group": "off",
   },
 };
