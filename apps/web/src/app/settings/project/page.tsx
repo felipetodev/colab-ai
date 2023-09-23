262f2b74bebe5076736663a2dfde8a6f224632f6
felipetodev <fe.ossandon.u@gmail.com>
2023-09-23 15:40:32 -0300

    add settings components

---
+++ apps/web/src/app/settings/project/page.tsx
@@ -0,0 +1,15 @@
+export default function ProjectSettings() {
+  return (
+    <main className="space-y-6">
+      <div>
+        <h3 className="text-lg font-medium">Project Settings</h3>
+        <p className="text-sm text-muted-foreground">
+          Data base settings, API keys, and other project-specific settings.
+        </p>
+        <p className="mt-5 text-sm text-muted-foreground">
+          (Under construction) 🚧
+        </p>
+      </div>
+    </main>
+  );
+}
