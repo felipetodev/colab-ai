ca94603d2b9faadaed66d2ebdb30d9c1a4eafec3
felipetodev <fe.ossandon.u@gmail.com>
2023-09-25 16:42:57 -0300

    refact database types with chat ai sdk types

--- apps/web/src/lib/types/chat.ts
+++ apps/web/src/lib/types/chat.ts
@@ -1,3 +1,9 @@
+import type { Message} from "ai/react";
 import type { Tables } from "./database";
 
-export type Chat = Tables<'chats'>
+export type Chat = {
+  id: Tables<'chats'>['id']
+  name: Tables<'chats'>['name']
+  messages: Message[]
+  folderId: Tables<'chats'>['folder_id']
+}
