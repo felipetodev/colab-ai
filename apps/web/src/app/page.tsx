ee492a9cd2eddd20bf87f7196238a68d070e062d
felipetodev <fe.ossandon.u@gmail.com>
2023-09-25 22:43:28 -0300

    feat: add new columns in chat with llm settings

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -7,7 +7,7 @@ export default async function Home() {
   const supabase = createServerComponentClient<Database>({ cookies })
   const { data: chats } = await supabase
     .from('chats')
-    .select('id, name, messages, folderId:folder_id')
+    .select('id, name, messages, folderId:folder_id, model, temperature, maxTokens:max_tokens')
 
   return (
     <main className="flex-col flex h-[calc(100vh-57px)] min-w-[1280px] overflow-hidden">
