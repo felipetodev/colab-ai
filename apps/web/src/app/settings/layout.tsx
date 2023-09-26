0b569f7e59af24e5bfacf26231b7db28a0c8a951
felipetodev <fe.ossandon.u@gmail.com>
2023-09-26 10:05:05 -0300

    fix: layout unused mobile view

--- apps/web/src/app/settings/layout.tsx
+++ apps/web/src/app/settings/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next"
-import Image from "next/image"
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
 import { cookies } from "next/headers"
 import { redirect } from "next/navigation";
@@ -39,45 +38,27 @@ export default async function SettingsLayout({ children }: SettingsLayoutProps) 
   }
 
   return (
-    <>
-      <div className="md:hidden">
-        <Image
-          alt="Forms"
-          className="block dark:hidden"
-          height={791}
-          src="/examples/forms-light.png"
-          width={1280}
-        />
-        <Image
-          alt="Forms"
-          className="hidden dark:block"
-          height={791}
-          src="/examples/forms-dark.png"
-          width={1280}
-        />
+    <div className="space-y-6 p-10 pb-16 md:block">
+      <div className="space-y-0.5">
+        <h2 className="text-2xl font-bold tracking-tight">Settings</h2>
+        <p className="text-muted-foreground">
+          Manage your account settings and set e-mail preferences.
+        </p>
       </div>
-      <div className="hidden space-y-6 p-10 pb-16 md:block">
-        <div className="space-y-0.5">
-          <h2 className="text-2xl font-bold tracking-tight">Settings</h2>
-          <p className="text-muted-foreground">
-            Manage your account settings and set e-mail preferences.
-          </p>
-        </div>
-        <Separator className="my-6" />
-        <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
-          <aside className="-mx-4 lg:w-1/5">
-            <SidebarNav items={sidebarNavItems} />
-            <div className="mt-3 cursor-not-allowed">
-              <Button disabled variant='destructive'>
-                Delete project
-              </Button>
-            </div>
-          </aside>
-          <div className="flex-1 lg:max-w-2xl">
-            {children}
+      <Separator className="my-6" />
+      <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
+        <aside className="-mx-4 lg:w-1/5">
+          <SidebarNav items={sidebarNavItems} />
+          <div className="mt-3 cursor-not-allowed">
+            <Button disabled variant='destructive'>
+              Delete project
+            </Button>
           </div>
+        </aside>
+        <div className="flex-1 lg:max-w-2xl">
+          {children}
         </div>
       </div>
-    </>
+    </div>
   )
 }
