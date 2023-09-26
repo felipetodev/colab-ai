f672f633ef3b78647c055aa93f1ad3b4719d93b3
felipetodev <fe.ossandon.u@gmail.com>
2023-09-25 22:45:38 -0300

    update types

--- apps/web/src/lib/types/chat.ts
+++ apps/web/src/lib/types/chat.ts
@@ -6,4 +6,8 @@ export type Chat = {
   name: Tables<'chats'>['name']
   messages: Message[]
   folderId: Tables<'chats'>['folder_id']
+  model?: Tables<'chats'>['model']
+  temperature?: Tables<'chats'>['temperature']
+  maxTokens?: Tables<'chats'>['max_tokens']
+  prompt?: Tables<'chats'>['prompt']
 }
