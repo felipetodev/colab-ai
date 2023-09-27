d8f8b1b591786d66c70c8711f315108feadb8f5d
felipetodev <fe.ossandon.u@gmail.com>
2023-09-26 21:02:46 -0300

    feat: get new tables agent and documents

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -9,10 +9,20 @@ export default async function Home() {
     .from('chats')
     .select('id, name, messages, folderId:folder_id, model, temperature, maxTokens:max_tokens, prompt')
 
+  const { data: agents } = await supabase
+    .from('agents')
+    .select()
+
+  const { data: documents } = await supabase
+    .from('documents')
+    .select('id, folderId:folder_id, name, content, type, isTrained:is_trained, createdAt:created_at')
+
   return (
     <main className="flex-col flex h-[calc(100vh-57px)] min-w-[1280px] overflow-hidden">
       <Chat
+        agents={agents ?? []}
         chats={chats ?? []}
+        documents={documents ?? []}
         id={crypto.randomUUID()}
       />
     </main>
