45d3389a295095ed3397c208cad69c55356c0352
felipetodev <fe.ossandon.u@gmail.com>
2023-09-27 19:14:30 -0300

    feat: add avatar to agent dialog confg

--- apps/web/src/components/agent-dialog.tsx
+++ apps/web/src/components/agent-dialog.tsx
@@ -1,6 +1,6 @@
 import { DialogClose } from "@radix-ui/react-dialog"
 import { useState } from "react"
-import { ChevronRightIcon } from "lucide-react"
+import { ChevronRightIcon, ImagePlus } from "lucide-react"
 import {
   Dialog,
   DialogContent,
@@ -21,21 +21,43 @@ import MaxTokensSelector from "./max-tokens-selector"
 import { Label } from "./ui/label"
 
 type Props = {
-  agent: Chat // refact this
+  agent: Chat // refact this for AGENT type
   children: React.ReactNode
 }
 
 function AgentDialog({ agent, children }: Props) {
+  const [avatarPreview, setAvatarPreview] = useState<string | null>(null)
   const [isOpen, setIsOpen] = useState(false)
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   const onUpdateSelectedChat = () => { }
 
+  const handleAvatar = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files
+    if (!files || files.length === 0) return
+    const image = files[0]
+    const formData = new FormData()
+    formData.append('avatar', image)
+
+    // show avatar preview before upload
+    const reader = new FileReader()
+    reader.onloadend = () => {
+      setAvatarPreview(reader.result as string)
+    }
+    reader.readAsDataURL(image)
+
+    // upload avatar to storage reference agent
+  }
+
   return (
-    <Dialog>
+    <Dialog onOpenChange={(open) => {
+      if (!open) {
+        setAvatarPreview(null)
+      }
+    }}>
       <DialogTrigger asChild>
         {children}
       </DialogTrigger>
-      <DialogContent className="sm:max-w-2xl">
+      <DialogContent className="sm:max-w-2xl overflow-y-auto max-h-[780px]">
         <DialogHeader>
           <DialogTitle>
             [Agent]: {agent.name}
@@ -45,27 +67,52 @@ function AgentDialog({ agent, children }: Props) {
           </DialogDescription>
         </DialogHeader>
         <div className="flex flex-col gap-y-3">
-          <div>
+          <div className="space-y-2">
             <Label htmlFor="name">Name</Label>
             <Input id="name" placeholder="Agent Name" readOnly value={agent.name} />
           </div>
-          <div>
+
+          <div className="space-y-2 flex flex-col justify-center">
+            <Label htmlFor="dropzone-file">Avatar</Label>
+            <Label className="flex flex-col items-center justify-center w-20 h-20 border rounded cursor-pointer overflow-hidden" htmlFor="dropzone-file">
+              {avatarPreview ? (
+                <img
+                  alt="avatar"
+                  className="block h-full w-full object-cover"
+                  src={avatarPreview}
+                />
+              ) : (
+                <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                  <ImagePlus className="w-5 h-5" />
+                </div>
+              )}
+              <input
+                accept="image/jpeg, image/png, image/webp, image/jpg"
+                className="hidden"
+                id="dropzone-file"
+                onChange={handleAvatar}
+                type="file"
+              />
+            </Label>
+          </div>
+
+          <div className="space-y-2">
             <Label htmlFor="upload">Choose document</Label>
             <div className="text-destructive font-semibold">
               HERE GOES THE FILE UPLOAD TO TRAIN THE AGENT
             </div>
           </div>
-          <div>
+          <div className="space-y-2">
             <Label htmlFor="prompt">Prompt</Label>
             <Textarea className="h-40" id="prompt" placeholder="Write your instructions here..." readOnly value={agent.prompt} />
           </div>
 
-          <div>
+          <div className="space-y-2">
             <Label htmlFor="model">Model</Label>
             <ModelSelector onChange={onUpdateSelectedChat} />
           </div>
 
-          <div className="w-full">
+          <div className="w-full mt-2">
             <Button
               className="flex items-center justify-start mb-4"
               onClick={() => setIsOpen(!isOpen)}
