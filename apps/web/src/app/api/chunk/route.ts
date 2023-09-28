18e0a895250432d9a205072efcb9cb2780f68769
felipetodev <fe.ossandon.u@gmail.com>
2023-09-28 15:58:45 -0300

    fix: add uuid in docs table and metadata of chunked doc to ref later

--- apps/web/src/app/api/chunk/route.ts
+++ apps/web/src/app/api/chunk/route.ts
@@ -1,3 +1,4 @@
+import crypto from 'node:crypto'
 import { NextResponse } from "next/server";
 import { WebPDFLoader } from "langchain/document_loaders/web/pdf";
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
@@ -9,19 +10,25 @@ export async function POST(request: Request) {
   const pdf = formData.get('file') as Blob | null
   const type = formData.get('type') as string | null
   const name = formData.get('name') as string | null
+  const documentId = crypto.randomUUID()
 
   if (!pdf) return null
 
   const loader = new WebPDFLoader(pdf);
   const document = await loader.load();
 
+  document.forEach((page) => {
+    page.metadata.id = documentId
+  })
+
   const supabase = createServerComponentClient({ cookies })
   const { data: { user } } = await supabase.auth.getUser()
 
-  if (user === null) return
+  if (user === null) return NextResponse.json({ error: 'User not found' })
 
   await supabase.from('documents').insert({
     user_id: user.id,
+    id: documentId,
     name,
     type,
     content: document
