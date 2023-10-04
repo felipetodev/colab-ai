acd5ca978a0a66ad310075d54ee19edb488731b8
felipetodev <fe.ossandon.u@gmail.com>
2023-10-04 13:13:56 -0300

    fix: refact supabase api on edge update documents

--- apps/web/src/app/api/embeddings/pinecone/route.ts
+++ apps/web/src/app/api/embeddings/pinecone/route.ts
@@ -4,12 +4,6 @@ import { NextResponse } from 'next/server'
 import { chunkedUpsert, embedDocument } from 'src/services/pinecone/utils'
 import { supabase } from 'src/services/supabase/api'
 
-const {
-  SUPABASE_SECRET_KEY,
-  SUPABASE_PROJECT_URL,
-  SUPABASE_ANON_KEY
-} = process.env
-
 export const runtime = 'edge'
 export const dynamic = 'force-dynamic'
 
@@ -70,18 +64,13 @@ export async function POST (req: Request) {
   // Upsert vectors into the Pinecone index
   await chunkedUpsert(index, vectors, '', 10)
 
-  await fetch(`${SUPABASE_PROJECT_URL}/rest/v1/documents?id=eq.${docId}`, {
-    method: 'PATCH',
-    headers: {
-      'Content-Type': 'application/json',
-      apikey: SUPABASE_ANON_KEY!,
-      Authorization: `Bearer ${SUPABASE_SECRET_KEY}`
-    },
-    body: JSON.stringify({
+  await supabase.updateDocuments({
+    id: docId,
+    update: {
       name,
       is_trained: true,
       embeddings_ids: ids
-    })
+    }
   })
 
   return NextResponse.json({ finished: true, name, ids })
@@ -117,17 +106,12 @@ export async function DELETE (req: Request) {
   const index = pinecone.Index(pineconeIndex)
   await index.deleteMany(ids)
 
-  await fetch(`${SUPABASE_PROJECT_URL}/rest/v1/documents?id=eq.${docId}`, {
-    method: 'PATCH',
-    headers: {
-      'Content-Type': 'application/json',
-      apikey: SUPABASE_ANON_KEY!,
-      Authorization: `Bearer ${SUPABASE_SECRET_KEY}`
-    },
-    body: JSON.stringify({
+  await supabase.updateDocuments({
+    id: docId,
+    update: {
       is_trained: false,
       embeddings_ids: null
-    })
+    }
   })
 
   return NextResponse.json({ delete: true })
