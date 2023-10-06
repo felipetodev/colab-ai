6d87f7f7061258901ce5b93b6d84a31078a74b8f
felipetodev <fe.ossandon.u@gmail.com>
2023-10-06 00:08:57 -0300

    fix: add pinecone similarity search from langchain

--- apps/web/src/app/api/completions/pinecone/route.ts
+++ apps/web/src/app/api/completions/pinecone/route.ts
@@ -1,5 +1,4 @@
 import type { Message as VercelChatMessage } from 'ai'
-import { getEmbeddings } from '@/lib/utils.edge'
 import { PromptTemplate } from 'langchain/prompts'
 import { ChatOpenAI } from 'langchain/chat_models/openai'
 import { BytesOutputParser } from 'langchain/schema/output_parser'
@@ -7,7 +6,9 @@ import { StreamingTextResponse } from 'ai'
 import { type AgentProps } from '@/lib/types/agent'
 import { RequestCookies } from '@edge-runtime/cookies'
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
-import { getMatchesFromEmbeddings } from 'src/services/pinecone/utils'
+import { Pinecone } from '@pinecone-database/pinecone'
+import { PineconeStore } from 'langchain/vectorstores/pinecone'
+import { OpenAIEmbeddings } from 'langchain/embeddings/openai'
 
 export const runtime = 'edge'
 
@@ -19,15 +20,20 @@ export type Metadata = {
 
 const TEMPLATE = `You are a representative who loves to help people!
 Given the following sections from the documentation (preceded by a section id), answer the question using only that information, outputted in Markdown format.
-
-Here is some context which might contain valuable information to answer the question:
 {prompt}
 
 Current conversation:
 {chat_history}
 
+Here is some context which might contain valuable information to answer the question:
+{context}
+
 User: {input}
 AI:`
+
+const formatMessage = (message: VercelChatMessage) => {
+  return `${message.role}: ${message.content}`
+}
 
 export async function POST (req: Request) {
   const body = await req.json() as {
@@ -39,7 +45,7 @@ export async function POST (req: Request) {
     maxTokens?: AgentProps['maxTokens']
   }
   const messages = body.messages ?? []
-  // const formattedPreviousMessages = messages.slice(0, -1).map(formatMessage)
+  const formattedPreviousMessages = messages.slice(0, -1).map(formatMessage)
   const currentMessageContent = messages[messages.length - 1].content
 
   const { docsId, prompt: agentPrompt, model: agentModel, temperature } = body
@@ -70,31 +76,35 @@ export async function POST (req: Request) {
     throw new Error('Missing Pinecone ⚡ credentials')
   }
 
-  const embedding = await getEmbeddings(
-    currentMessageContent,
-    {
-      apiKey: openaiKey,
+  const pinecone = new Pinecone({
+    apiKey: pineconeApiKey,
+    environment: pineconeEnvironment
+  })
+
+  const index = pinecone.Index(pineconeIndex)
+
+  const vectorStore = await PineconeStore.fromExistingIndex(
+    new OpenAIEmbeddings({
+      openAIApiKey: openaiKey
+    }, {
       ...(openaiOrg && { organization: openaiOrg })
+    }),
+    {
+      pineconeIndex: index
     }
   )
 
-  const matches = await getMatchesFromEmbeddings({
-    embeddings: embedding,
-    topK: 3,
-    indexName: pineconeIndex,
-    settings: {
-      apiKey: pineconeApiKey,
-      environment: pineconeEnvironment
-    },
-    filterIds: docsId ?? []
-  })
-
-  // const minScore = 0.7
-  // const qualifyingDocs = matches.filter(m => m.score && m.score > minScore)
-  const content = matches ? matches.map(match => (match.metadata as Metadata).chunk).join('\n') : ''
+  const results = await vectorStore.similaritySearch(
+    currentMessageContent,
+    3,
+    {
+      id: { $in: docsId }
+    }
+  )
 
   console.log('------------------')
-  console.log(content)
+  console.log(results)
+  console.log({ references: docsId })
   console.log('------------------')
 
   const prompt = PromptTemplate.fromTemplate(TEMPLATE)
@@ -114,7 +124,8 @@ export async function POST (req: Request) {
 
   const stream = await chain.stream({
     prompt: agentPrompt ?? '',
-    chat_history: content,
+    chat_history: formattedPreviousMessages.join('\n'),
+    context: results.map(({ pageContent }) => pageContent).join('\n'),
     input: currentMessageContent
   })
 
--- apps/web/src/app/api/embeddings/pinecone/route.ts
+++ apps/web/src/app/api/embeddings/pinecone/route.ts
@@ -1,12 +1,12 @@
 import { type DocumentProps } from '@/lib/types/document'
 import { Pinecone } from '@pinecone-database/pinecone'
 import { NextResponse } from 'next/server'
-import { chunkedUpsert, embedDocument } from 'src/services/pinecone/utils'
 import { RequestCookies } from '@edge-runtime/cookies'
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { OpenAIEmbeddings } from 'langchain/embeddings/openai'
+import { PineconeStore } from 'langchain/vectorstores/pinecone'
 
 export const runtime = 'edge'
-export const dynamic = 'force-dynamic'
 
 export async function POST (req: Request) {
   const { name, docId, content } = await req.json() as {
@@ -44,42 +44,27 @@ export async function POST (req: Request) {
     environment: pineconeEnvironment
   })
 
-  const indexList = await pinecone.listIndexes()
-
-  const indexExists = indexList.some(index => index.name === pineconeIndex)
-  if (!indexExists) {
-    await pinecone.createIndex({
-      name: pineconeIndex,
-      dimension: 1536,
-      waitUntilReady: true
-    })
-  }
-
   const index = pinecone.Index(pineconeIndex)
 
-  const vectors = await Promise.all(content.flat().map(
-    async (e) => await embedDocument(
-      e,
-      {
-        apiKey: openaiKey,
-        ...(openaiOrg && { organization: openaiOrg })
-      }
-    )
-  ))
-  const ids = vectors.map(({ id }) => id)
-
-  // Upsert vectors into the Pinecone index
-  await chunkedUpsert(index, vectors, '', 10)
+  await PineconeStore.fromDocuments(
+    content,
+    new OpenAIEmbeddings({
+      openAIApiKey: openaiKey
+    }, {
+      ...(openaiOrg && { organization: openaiOrg })
+    }), {
+      pineconeIndex: index
+    })
 
   await supabase.from('documents')
     .update({
       name,
       is_trained: true,
-      embeddings_ids: ids
+      embeddings_ids: ['todo'] // fix this later ☝
     })
     .eq('id', docId)
 
-  return NextResponse.json({ finished: true, name, ids })
+  return NextResponse.json({ finished: true })
 }
 
 export async function DELETE (req: Request) {
