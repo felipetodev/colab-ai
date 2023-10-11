30156dbac31237c8435290397d732867db2638e5
felipetodev <fe.ossandon.u@gmail.com>
2023-10-10 21:57:33 -0300

    feat: add trained badge and github icon

--- apps/web/src/components/document-file.tsx
+++ apps/web/src/components/document-file.tsx
@@ -2,6 +2,8 @@ import { Settings, FileText } from 'lucide-react'
 import type { DocumentProps } from '@/lib/types/document'
 import { Button } from './ui/button'
 import DocumentPreviewDialog from './document-preview-dialog'
+import { Badge } from './ui/badge'
+import { IconGitHub } from './ui/icons'
 
 type Props = {
   document: DocumentProps
@@ -28,12 +30,25 @@ function DocumentFile ({ document, user }: Props) {
         role="button"
         tabIndex={0}
       >
-        <span className="mr-2 p-2 rounded-md">
+        {document.type === 'github'
+          ? (
+          <span className="mr-2 p-2 rounded-md">
+            <IconGitHub className='w-6 h-6' />
+          </span>
+            )
+          : (
+          <span className="mr-2 p-2 rounded-md">
           <FileText />
         </span>
-        <span className="text-sm truncate w-full mr-2">
+            )}
+        <span className="text-sm truncate mr-2">
           {document.name}
         </span>
+        {document.isTrained && (
+          <Badge variant='outline'>
+            trained
+          </Badge>
+        )}
         <div className="ml-auto flex">
           <Button className="w-7 h-7" size='icon' variant="ghost">
             <Settings className="w-4 h-4" />
