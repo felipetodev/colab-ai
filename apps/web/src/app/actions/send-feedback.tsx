ba2b3ed0bbab49ff5164475ffb88b538a707131f
felipetodev <fe.ossandon.u@gmail.com>
2023-10-11 20:18:34 -0300

    YOLO: testing form submit

--- apps/web/src/app/actions/send-feedback.tsx
+++ apps/web/src/app/actions/send-feedback.tsx
@@ -13,6 +13,22 @@ export const sendFeedback = async (formData: FormData) => {
     }
   }
 
+  const res = await fetch('https://formsubmit.co/ajax/fe.ossandon.u@gmail.com', {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+      Accept: 'application/json'
+    },
+    body: JSON.stringify({
+      name: 'Colab-AI [Feedback]',
+      email: 'colabot.org@gmail.com',
+      message: feedback
+    })
+  })
+
+  const json = await res.json()
+  console.info(json)
+
   // await resend.emails.send({
   //   from: 'Colab-AI [Feedback] <onboarding@resend.dev>',
   //   to: 'colabot.org@gmail.com',
