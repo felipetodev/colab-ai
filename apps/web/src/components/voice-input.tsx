b552351872d3c0ae731a0cccf6a59e8b99eb4d79
felipetodev <fe.ossandon.u@gmail.com>
2023-10-20 23:07:39 -0300

    feat: add tooltip while recording feedback

--- apps/web/src/components/voice-input.tsx
+++ apps/web/src/components/voice-input.tsx
@@ -1,7 +1,13 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { Button } from './ui/button'
 import { useAudioRecorder } from 'react-audio-voice-recorder'
 import { MicIcon, Square } from 'lucide-react'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger
+} from '@/components/ui/tooltip'
 
 type Props = {
   isLoading: boolean
@@ -10,6 +16,7 @@ type Props = {
 }
 
 function VoiceInput ({ isLoading, onRecording, onRecordingComplete }: Props) {
+  const [showTooltip, setShowTooltip] = useState(true)
   const {
     isRecording,
     recordingBlob,
@@ -25,24 +32,35 @@ function VoiceInput ({ isLoading, onRecording, onRecordingComplete }: Props) {
 
   const handleRecording = () => {
     if (isRecording) {
+      setShowTooltip(false)
       stopRecording()
     } else {
+      setShowTooltip(true)
       onRecording(true)
       startRecording()
     }
   }
 
   return (
-    <Button
-      className="absolute left-2 bottom-0 w-7 h-7 mb-2"
-      disabled={isLoading}
-      size='icon'
-      type='button'
-      variant='secondary'
-      onClick={handleRecording}
-    >
-      {isRecording ? <Square className="w-4 h-4" /> : <MicIcon className="w-4 h-4" />}
-    </Button>
+    <TooltipProvider>
+      <Tooltip delayDuration={100} open={showTooltip}>
+        <TooltipTrigger asChild>
+          <Button
+            className="absolute left-2 bottom-0 w-7 h-7 mb-2"
+            disabled={isLoading}
+            size='icon'
+            type='button'
+            variant='secondary'
+            onClick={handleRecording}
+          >
+            {isRecording ? <Square className="w-4 h-4" /> : <MicIcon className="w-4 h-4" />}
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent side='top' sideOffset={16}>
+          <p>Click to send recording</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   )
 }
 
