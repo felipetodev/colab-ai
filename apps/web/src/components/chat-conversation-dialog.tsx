44de7585112f18ed2cb713bb6cb4aa5d7f093d78
felipetodev <fe.ossandon.u@gmail.com>
2023-10-31 16:55:58 -0300

    fix: add tooltip feedback in chat bubble options

--- apps/web/src/components/chat-conversation-dialog.tsx
+++ apps/web/src/components/chat-conversation-dialog.tsx
@@ -5,36 +5,40 @@ import { Button, buttonVariants } from './ui/button'
 import { useToast } from './ui/use-toast'
 import { deleteChat, updateChat } from 'src/app/actions/chat'
 import { useRouter } from 'next/navigation'
+import { FileEditIcon, Trash2Icon } from 'lucide-react'
 import { Spinner } from './ui/icons'
 import {
   AlertDialog,
   AlertDialogCancel,
-  AlertDialogTrigger,
   AlertDialogContent,
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogDescription,
   AlertDialogTitle
 } from './ui/alert-dialog'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger
+} from './ui/tooltip'
 
 type Props =
   | {
     type: 'delete'
     id: string
     isAgent?: boolean
-    children: React.ReactNode
   } | {
     type: 'edit'
     activeName: string
     isAgent: boolean
     id: string
-    children: React.ReactNode
   }
 
 function ChatConversationDialog (props: Props) {
   const [isOpen, setIsOpen] = useState(false)
   const [isRemovePending, startRemoveTransition] = useTransition()
-  const { type, id, isAgent, children } = props
+  const { type, id, isAgent } = props
   const inputRef = useRef<HTMLInputElement>(null)
   const router = useRouter()
   const { toast } = useToast()
@@ -70,68 +74,96 @@ function ChatConversationDialog (props: Props) {
   }
 
   return (
-    <AlertDialog open={isOpen} onOpenChange={setIsOpen}>
-      <AlertDialogTrigger asChild>
-        {children}
-      </AlertDialogTrigger>
-      <AlertDialogContent className="sm:max-w-xl">
-        {type === 'edit'
-          ? (
-            <>
-              <AlertDialogHeader>
-                <AlertDialogTitle>Edit chat</AlertDialogTitle>
-                <AlertDialogDescription>
-                  Update your chat name.
-                </AlertDialogDescription>
-              </AlertDialogHeader>
-              <div className="grid gap-4 py-4">
-                <Input ref={inputRef} name="name" placeholder={props.activeName} className='truncate' />
-              </div>
-            </>
-            )
-          : (
-            <AlertDialogHeader>
-              <AlertDialogTitle>Delete chat</AlertDialogTitle>
-              <AlertDialogDescription>
-                Are you sure you want to delete this chat?
-                This will permanently delete your chat message and remove your
-                data from our servers.
-              </AlertDialogDescription>
-            </AlertDialogHeader>
-            )}
-        <AlertDialogFooter>
-          {type === 'delete'
+    <>
+      {type === 'edit'
+        ? (
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button onClick={() => setIsOpen(true)} className="w-7 h-7" size='icon' variant="ghost">
+                  <FileEditIcon className="w-4 h-4" />
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent>
+                Edit chat name
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+          )
+        : (
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button onClick={() => setIsOpen(true)} className="w-7 h-7" size='icon' variant="ghost">
+                  <Trash2Icon className="w-4 h-4" />
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent>
+                Delete chat
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+          )}
+      <AlertDialog open={isOpen} onOpenChange={setIsOpen}>
+        <AlertDialogContent className="sm:max-w-xl">
+          {type === 'edit'
             ? (
               <>
-                <AlertDialogCancel disabled={isRemovePending} className={cn(buttonVariants({ variant: 'secondary' }))}>
-                  Cancel
-                </AlertDialogCancel>
-                <Button
-                  disabled={isRemovePending}
-                  variant='destructive'
-                  onClick={handleDeleteChat}
-                >
-                  {isRemovePending ? <Spinner className='animate-spin w-5 h-5' /> : 'Delete'}
-                </Button>
+                <AlertDialogHeader>
+                  <AlertDialogTitle>Edit chat</AlertDialogTitle>
+                  <AlertDialogDescription>
+                    Update your chat name.
+                  </AlertDialogDescription>
+                </AlertDialogHeader>
+                <div className="grid gap-4 py-4">
+                  <Input ref={inputRef} name="name" placeholder={props.activeName} className='truncate' />
+                </div>
               </>
               )
             : (
-              <>
-                <AlertDialogCancel disabled={isRemovePending} className={cn(buttonVariants({ variant: 'secondary' }))}>
-                  Cancel
-                </AlertDialogCancel>
-                <Button
-                  disabled={isRemovePending}
-                  className='text-white bg-green-700 hover:bg-green-700/90'
-                  onClick={handleUpdateChat}
-                >
-                  {isRemovePending ? <Spinner className='animate-spin w-5 h-5' /> : 'Save'}
-                </Button>
-              </>
+              <AlertDialogHeader>
+                <AlertDialogTitle>Delete chat</AlertDialogTitle>
+                <AlertDialogDescription>
+                  Are you sure you want to delete this chat?
+                  This will permanently delete your chat message and remove your
+                  data from our servers.
+                </AlertDialogDescription>
+              </AlertDialogHeader>
               )}
-        </AlertDialogFooter>
-      </AlertDialogContent>
-    </AlertDialog>
+          <AlertDialogFooter>
+            {type === 'delete'
+              ? (
+                <>
+                  <AlertDialogCancel disabled={isRemovePending} className={cn(buttonVariants({ variant: 'secondary' }))}>
+                    Cancel
+                  </AlertDialogCancel>
+                  <Button
+                    disabled={isRemovePending}
+                    variant='destructive'
+                    onClick={handleDeleteChat}
+                  >
+                    {isRemovePending ? <Spinner className='animate-spin w-5 h-5' /> : 'Delete'}
+                  </Button>
+                </>
+                )
+              : (
+                <>
+                  <AlertDialogCancel disabled={isRemovePending} className={cn(buttonVariants({ variant: 'secondary' }))}>
+                    Cancel
+                  </AlertDialogCancel>
+                  <Button
+                    disabled={isRemovePending}
+                    className='text-white bg-green-700 hover:bg-green-700/90'
+                    onClick={handleUpdateChat}
+                  >
+                    {isRemovePending ? <Spinner className='animate-spin w-5 h-5' /> : 'Save'}
+                  </Button>
+                </>
+                )}
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </>
   )
 }
 
--- apps/web/src/components/chat-conversation.tsx
+++ apps/web/src/components/chat-conversation.tsx
@@ -1,7 +1,6 @@
-import { MessageCircleIcon, Trash2Icon, FileEditIcon } from 'lucide-react'
+import { MessageCircleIcon } from 'lucide-react'
 import type { Chat } from '@/lib/types/chat'
 import { cn } from '@/lib/utils'
-import { Button } from '@/components/ui/button'
 import ChatConversationDialog from './chat-conversation-dialog'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
@@ -30,26 +29,20 @@ function ChatConversation ({ id, name, isAgent }: Props) {
       <span className="text-sm truncate w-full mr-2">
         {name}
       </span>
-      <div className="ml-auto flex">
+      <div className={cn('ml-auto hidden', {
+        flex: pathname === chatPath
+      })}>
         <ChatConversationDialog
           activeName={name}
           isAgent={isAgent}
           id={id}
           type="edit"
-        >
-          <Button className="w-7 h-7" size='icon' variant="ghost">
-            <FileEditIcon className="w-4 h-4" />
-          </Button>
-        </ChatConversationDialog>
+        />
 
         <ChatConversationDialog
           id={id}
           type="delete"
-        >
-          <Button className="w-7 h-7" size='icon' variant="ghost">
-            <Trash2Icon className="w-4 h-4" />
-          </Button>
-        </ChatConversationDialog>
+        />
       </div>
     </Link>
   )
