c76c4d54fca45003eb65e8658f8e3e8da99d9247
felipetodev <fe.ossandon.u@gmail.com>
2023-11-06 19:32:52 -0300

    feat: update resend and add api

--- apps/web/src/app/actions/send-feedback.tsx
+++ apps/web/src/app/actions/send-feedback.tsx
@@ -1,8 +1,8 @@
 'use server'
 
-// import { Resend } from 'resend'
+import { Resend } from 'resend'
 
-// const resend = new Resend(process.env.RESEND_API_KEY)
+const resend = new Resend(process.env.RESEND_API_KEY)
 
 export const sendFeedback = async (formData: FormData) => {
   const feedback = formData.get('feedback')
@@ -13,28 +13,15 @@ export const sendFeedback = async (formData: FormData) => {
     }
   }
 
-  const res = await fetch('https://formsubmit.co/ajax/fe.ossandon.u@gmail.com', {
-    method: 'POST',
-    headers: {
-      'Content-type': 'application/json',
-      Accept: 'application/json'
-    },
-    body: JSON.stringify({
-      name: 'Colab-AI [Feedback]',
-      email: 'colabot.org@gmail.com',
-      message: feedback
-    })
+  const { data, error } = await resend.emails.send({
+    from: 'Colab-AI [Feedback] <colabot.org@gmail.com>',
+    to: 'colabot.org@gmail.com',
+    reply_to: 'fe.ossandon.u@gmail.com',
+    subject: 'Feedback',
+    text: feedback
   })
 
-  const json = await res.json()
-  console.info(json)
+  console.log({ data, error })
 
-  // await resend.emails.send({
-  //   from: 'Colab-AI [Feedback] <onboarding@resend.dev>',
-  //   to: 'colabot.org@gmail.com',
-  //   reply_to: 'fe.ossandon.u@gmail.com',
-  //   subject: 'Feedback',
-  //   text: feedback
-  // })
   return { success: true }
 }
